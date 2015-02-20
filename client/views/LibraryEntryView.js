0a25e7347234ef49bca7fb0eeced523ae5c9c586
yesplease <bradley.heinz@gmail.com>
2015-02-19 19:39:20 -0800

    Style

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -4,7 +4,7 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td class="playSong">(<%= artist %>)</td><td class="playSong"><%= title %></td><td class="queueSong">Queue</td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td> <img class = "playSong" src="https://android-ui-utils.googlecode.com/hg-history/9472134d092f3375aa9cf767d6a4b2eca561c0ba/asset-studio/dist/res/clipart/icons/av_add_to_queue.svg" height="42" width="42"></td>'),
 
   events: {
     'click .playSong': function() {
--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -2,10 +2,10 @@
 var SongQueueEntryView = Backbone.View.extend({
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td><img class="removeSong" src="http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/glossy-black-3d-buttons-icons-media/002119-glossy-black-3d-button-icon-media-a-media292-minus3.png" height="42" width="42"></td>'),
 
   events: {
-    'click': function() {
+    'click .removeSong': function() {
       this.model.dequeue();
     },
   },
